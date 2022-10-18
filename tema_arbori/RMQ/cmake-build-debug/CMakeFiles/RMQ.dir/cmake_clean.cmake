91c77c51968ae16406b24592d7bbe5565796f6d9
Burdusa Petru-Robert <petru20019@gmail.com>
2022-10-18 19:48:28 +0300

    Add files via upload

---
+++ tema_arbori/RMQ/cmake-build-debug/CMakeFiles/RMQ.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RMQ.dir/main.cpp.obj"
+  "RMQ.exe"
+  "RMQ.exe.manifest"
+  "RMQ.pdb"
+  "libRMQ.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RMQ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
