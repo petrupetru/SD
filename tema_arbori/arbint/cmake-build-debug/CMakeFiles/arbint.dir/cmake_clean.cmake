d16795dd4dc0a3d23513c4a38f7de161d44b910e
Burdusa Petru-Robert <petru20019@gmail.com>
2022-10-18 19:45:24 +0300

    Add files via upload

---
+++ tema_arbori/arbint/cmake-build-debug/CMakeFiles/arbint.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arbint.dir/main.cpp.obj"
+  "arbint.exe"
+  "arbint.exe.manifest"
+  "arbint.pdb"
+  "libarbint.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/arbint.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tema_arbori/arbint/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+ifstream in("arbint.in");
+ofstream out("arbint.out");
+
+int arbint[400005];
+int v;
+int n, q, op, x, y;
+
+void Update( int nod, int st, int dr, int pos, int val )
+{
+    if( st == dr )
+    {
+        arbint[nod] = val;
+        return;
+    }
+    int mid = st + ( dr - st )/2;
+    if(pos <= mid) Update( nod * 2, st, mid, pos, val );
+    else Update( nod * 2 + 1, mid + 1, dr, pos, val );
+    arbint[nod] = max( arbint[nod*2], arbint[nod*2+1] );
+}
+
+int Query( int nod, int st, int dr, int L, int R )
+{
+    if( L <= st && dr <= R )
+        return arbint[nod];
+    int mid = st + ( dr - st )/2;
+    int ans1,ans2;
+    ans1 = ans2 = 0;
+    if( L <= mid ) ans1 = Query( nod*2, st, mid, L, R );
+    if( mid < R ) ans2 = Query( nod*2+1, mid+1, dr, L, R);
+    return max(ans1, ans2 );
+}
+
+int main()
+{
+    in>>n>>q;
+    for( int i=1;i<=n;++i)
+    {
+        in >> v;
+        Update( 1, 1, n, i, v );
+    }
+    for( int i=1;i<=q;++i)
+    {
+        in >> op >> x >> y;
+        if( op == 0 )
+            out << Query( 1, 1, n, x, y ) << '\n';
+        else
+            Update( 1, 1, n, x, y );
+    }
+}
